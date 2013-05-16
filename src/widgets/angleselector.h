9dfaac9b4834dc546273cfc9cca7e1106ea61e23
romain.baugue@gmail.com <romain.baugue@gmail.com@979d0781-ff8e-3938-e5a1-a8d0a11b4cfe>
2013-05-16 22:53:54 +0000

    [fix] added comment for widget classes [new] added signal to differentiate value change from user selection of segment/angle

--- src/widgets/angleselector.h
+++ src/widgets/angleselector.h
@@ -10,6 +10,9 @@ class QGridLayout;
 class QLabel;
 class SegmentSelector;
 
+/**
+ * A widget to select an angle composed of two segments
+ */
 class AngleSelector : public QWidget
 {
     Q_OBJECT
@@ -17,28 +20,88 @@ class AngleSelector : public QWidget
 public:
     AngleSelector(QWidget * parent = 0);
     
+    /**
+     * Return the selected labels of the fixed segment of the angle
+     */
     QPair<QString, QString> fixed() const;
+    
+    /**
+     * Return the selected labels of the mobile segment of the angle
+     */
     QPair<QString, QString> mobile() const;
     
+    /**
+     * Tells if a valid angle has been selected, ie if both segment selectors have a selected value
+     */
+    bool valid() const;
+    
 public slots:
+    /**
+     * Set the fixed segment of the angle, or set it to none if value doesn't exists
+     */
     void setFixed(QPair<QString, QString> value);
+    
+    /**
+     * Set the points labels for the user to choose amongst
+     */
     void setItems(QList<QString> value);
+    
+    /**
+     * Set the segments selectors labels
+     */
     void setLabels(QString fixed, QString mobile);
+    
+    /**
+     * Set the mobile segment of the angle, or set it to none if value doesn't exists
+     */
     void setMobile(QPair<QString, QString> value);
     
 protected slots:
+    /**
+     * Internal behavior when the user select another item in one of the segments selectors
+     */
     void onSelectionChange();
     
+    /**
+     * Internal behavior when the value of one of the segments selectors change
+     */
+    void onValueChange();
+    
 signals:
+    /**
+     * Emitted whenever the user changed the selected angle. Won't trigger if the change is done programmatically.
+     */
     void selectionChanged(QPair<QString, QString> fixed, QPair<QString, QString> mobile);
     
+    /**
+     * Emitted whenever the value of the selected angle changed.
+     */
+    void valueChanged(QPair<QString, QString> fixed, QPair<QString, QString> mobile);
+    
 private:
+    /**
+     * The layout holding widget's content
+     */
     QGridLayout * _layout;
     
+    /**
+     * The label of the fixed segment selector
+     */
     QLabel * _fixedLabel;
+    
+    /**
+     * The label of the mobile segment selector
+     */
     QLabel * _mobileLabel;
     
+    /**
+     * The fixed segment selector
+     */
     SegmentSelector * _fixedSelector;
+    
+    /**
+     * The mobile segment selector
+     */
     SegmentSelector * _mobileSelector;
 };
 
--- src/widgets/motiondetectorwidget.h
+++ src/widgets/motiondetectorwidget.h
@@ -14,6 +14,9 @@ class QSpinBox;
 class QVBoxLayout;
 class Record;
 
+/**
+ * The motion detector widget provide graphical user interface for the motion detector tool. It embed an angle selector, a plotting widget, an user-correction mechanism that uses spinboxes, and a save button.
+ */
 class MotionDetectorWidget : public QWidget
 {
     Q_OBJECT
@@ -21,18 +24,48 @@ class MotionDetectorWidget : public QWidget
 public:
     MotionDetectorWidget(QWidget * parent = 0);
     
+    /**
+     * Return a pointer on the current record of the motion detector
+     */
     Record * record() const;
     
 public slots:
+    /**
+     * Run the detector using the currently selected angle
+     */
     void run();
+    
+    /**
+     * Save the results of the detection into a file.
+     * FIXME Saving is actually hard-coded into a static file. Needs to change this as soon as database is implemented
+     */
     void save();
+    
+    /**
+     * Set the pointer of the current record
+     */
     void setRecord(Record * record);
+    
+    /**
+     * Set the default selected angle
+     */
     void setDefaultAngle(QPair<QString, QString> fixed, QPair<QString, QString> mobile);
     
 protected slots:
-    void onBeginSpinBoxValueChanged(int value);
-    void onEndSpinBoxValueChanged(int value);
-    void onPeakSpinBoxValueChanged(int value);
+    /**
+     * Internal behavior of the detector when the begin spinbox's value change
+     */
+    void onBeginSpinBoxValueChange(int value);
+    
+    /**
+     * Internal behavior of the detector when the end spinbox's value change
+     */
+    void onEndSpinBoxValueChange(int value);
+    
+    /**
+     * Internal behavior of the detector when the peak spinbox's value change
+     */
+    void onPeakSpinBoxValueChange(int value);
 
 signals:
     void recordChanged(Record * record);
